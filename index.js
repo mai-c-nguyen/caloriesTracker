f8acf47556d93655b991f57420b863c791fb2bc6
Mai Nguyen <mai.nguyen.gl@gmail.com>
2022-03-28 23:18:23 +1100

    adding Class

--- index.js
+++ index.js
@@ -54,5 +54,30 @@ form.addEventListener("submit", (event) => {
     protein.value = "";
     fat.value = "";
   });
+export default class Todos {
+    // Do NOT modify the constructor
+    constructor() {
+        // we don't capture any parameters here
+        // we're defining an array of todos with two example todos
+        this.todos = [{
+            title: "Learn JavaScript",
+            category: "work"
+        }, {
+            title: "Meditate",
+            category: "personal"
+        }];
+    }
 
+    // TODO: define remaining instance methods
+    getAll() {
+        return this.todos;
+    }
+    getCount() {
+        return this.todos.length;
+    }
+    add(title, category) {
+           return this.todos.push({title, category});
 
+    }
+
+}
